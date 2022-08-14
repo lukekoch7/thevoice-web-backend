122c8367cc0eb409167388235e37e1b2bf71121f
Lukas Koch <lukas.koch@stud.uni-bamberg.de>
2022-08-14 15:51:06 +0200

    implement route to fetch rand username

--- index.ts
+++ index.ts
@@ -7,13 +7,29 @@ import bodyParser from "body-parser";
 
 const app: express.Application = express();
 
-const allowedOrigins = ['https://thevoice-web.herokuapp.com'];
+const allowedOrigins = ["https://thevoice-web.herokuapp.com"];
+
+const randUsernames = [
+  "Christian Lindner",
+  "Mia Julia",
+  "Mathemann",
+  "Kazoo kid",
+  "Money Boy",
+  "Haftbefehl",
+  "Bad Luck Brian",
+  "Angela Merkel",
+  "Celo & Abdi",
+  "Ikke Hüftgold",
+  "Farid Bang",
+  "Xavier Naidoo",
+];
+let randUsernameIndex = 0;
 
 const options: cors.CorsOptions = {
-  origin: allowedOrigins
+  origin: allowedOrigins,
 };
 app.use(cors(options));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 const port: number = parseInt(process.env.PORT ?? "80");
 
 let currentBets: Bet[] = [];
@@ -32,21 +48,30 @@ app.post("/placeBet", (req, res) => {
   if (!newBet) {
     res.sendStatus(400);
   } else {
-    if(newBet.player === "admin42"){
+    if (newBet.player === "admin42") {
       currentResults = calcResult(currentBets, newBet);
       currentBets = [];
       res.send("Result successfully processed!");
       return;
     }
-    currentBets = currentBets.filter((bet) => bet.player !== newBet.player)
+    currentBets = currentBets.filter((bet) => bet.player !== newBet.player);
     currentBets.push(newBet);
-    console.log(`${newBet.player} placed bet: ${JSON.stringify(newBet)}`)
+    console.log(`${newBet.player} placed bet: ${JSON.stringify(newBet)}`);
     res.send("Successfully placed bet!");
   }
 });
 
 app.get("/results", (req, res) => {
   res.json(currentResults);
+});
+
+app.get("/randUsername", (req, res) => {
+  if (randUsernameIndex > randUsernames.length - 1) {
+    randUsernameIndex = 0;
+  }
+  const userName = randUsernames[randUsernameIndex];
+  randUsernameIndex++;
+  res.send(userName);
 });
 
 // Server setup
